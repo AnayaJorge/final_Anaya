a926dd8d7ea5a98edbaf9c84ba8e5fa816e3203a
Jorge Anaya <jeanaya@outlook.com>
2022-11-10 20:16:51 -0300

    carga del proyecto final

--- assets/js/financiacion.js
+++ assets/js/financiacion.js
@@ -47,95 +47,117 @@ class Boleto {
 
 const agregar = () => {
     //variable para alojar el retorno de la funcion del objeto.
+    Swal.fire ( {
+        title: '¿Desea cargar un nuevo Boleto?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Si quiero',
+        cancelButtonText: 'No quiero'
+    })
+
+// Solicitamos el número de Unidad
+.then((result) => {
+    
+    if(result.isConfirmed){
         Swal.fire ( {
-            title: '¿Desea cargar un nuevo Boleto?',
+            title: 'Ingrese el número de la unidad',
+            input: 'text',
             showCancelButton: true,
-            confirmButtonText: 'Si quiero',
-            cancelButtonText: 'No quiero'
-        }).then((result) => {
-            if(result.isConfirmed){
-                let unidad = parseInt( prompt('Ingrese el número de la unidad',0) );
-                verificarUnidad(unidad);
-                    function verificarUnidad (num){
-                        if (!isNaN(num) && num>0){
-                            //console.log ('El valor : ' + num + ' es un número')
-                        }
-                        
-                        else{
-                            mensaje = alert('Debes ingresar un numero');
-                            num = parseInt ( prompt('Ingrese el número de la unidad',0) );
-                            unidad = num
-                            verificarUnidad(num);
-                        }
-                    }
-                
-                let cliente = prompt('Ingrese el Apellido y Nombre del Cliente');
-                verificarNombre (cliente);
-                    function verificarNombre (nombre){
-                        if (nombre.length >= 1){
-                            //console.log ('El valor : ' + num + ' es un número')
-                        }
-                        
-                        else{
-                            mensaje = alert('Debes ingresar un cliente');
-                            nombre = prompt('Ingrese el Apellido y Nombre del Clientee',0);
-                            cliente = nombre
-                            verificarNombre(nombre);
-                        }
-                    }
-
-
-                let montoFinanciar = parseInt ( prompt('Ingrese el Monto a Financiar',0) );
-                verificarMontoFinanciar(montoFinanciar);
-                    function verificarMontoFinanciar (num){
-                        if (!isNaN(num) && num>0){
-                            //console.log ('El valor : ' + num + ' es un número')
-                        }
-                        
-                        else{
-                            mensaje = alert('Debes ingresar un numero');
-                            let num = parseInt ( prompt('Ingrese el Monto a Financiar',0) );
-                            montoFinanciar = num
-                            verificarMontoFinanciar (num);
-                        }
-                    }
-                    
-                let numeroCuotas = parseInt ( prompt('Ingrese la Cantidad de Cuotas',1) );
-                verificarNumeroCuotas(numeroCuotas);
-                    function verificarNumeroCuotas (num){
-                        if (!isNaN(num) && num>0){
-                            //console.log ('El valor : ' + num + ' es un número')
-                        }
-                        
-                        else{
-                            mensaje = alert('Debes ingresar un numero');
-                            let num = parseInt ( prompt('Ingrese la Cantidad de Cuotas',0) );
-                            numeroCuotas = num
-                            verificarNumeroCuotas (num);
-                        }
-                    }
-
-                    const objetoUnidad = unidades.find (producto =>{
-                        return producto.numeroUnidad === unidad;
-                    })
-                
-                archivoBoletos = JSON.parse(localStorage.getItem('boletos'));
-
-                let nuevoBoleto = new Boleto (generarBoleto(), objetoUnidad, cliente, montoFinanciar, numeroCuotas)
-                archivoBoletos.push(nuevoBoleto); 
-                sincronizarConLocalStorage(archivoBoletos)
-                representaArreglo(archivoBoletos);
-                
-                console.log('Financiación: ')
-                console.log(nuevoBoleto)
-                for( let i=1; i<=numeroCuotas; i++){
-                    let numeroCuota = i
-                    let valorCuota = (montoFinanciar / numeroCuotas)* (1+interes) ** (i-1);
-                    console.log('Nº Cuota: '+ numeroCuota + ' / Valor de Cuota: ' + valorCuota.toFixed(2))
+            inputValidator: (unidad) => {
+                let numeroUnidad = parseInt(unidad)
+                const validarUnidad = unidades.some( producto => producto.numeroUnidad === numeroUnidad )
+                if(!numeroUnidad || isNaN(numeroUnidad) || numeroUnidad<0 || validarUnidad == false){
+                    return 'Debes escribir un número o unidad valida'
                 }
-                confirm("La financiación se muestra por consola")
             }
         })
+
+// Solicitamos Nombre del Cliente
+.then((result) => {
+    if(result.isConfirmed){
+        let numeroUnidad = parseInt( result.value );
+        Swal.fire ( {
+            title: 'Ingrese el Apellido y Nombre del Cliente',
+            input: 'text',
+            showCancelButton: true,
+            inputValidator: (nombre) => {
+                if(!nombre){
+                    return 'Debes escribir un nombre'
+                }
+            }
+        })
+
+// Solicitamos el monto a Financiar
+.then((result) => {
+    if(result.isConfirmed){
+        let nombre = result.value;
+        Swal.fire ( {
+            title: 'Ingrese el monto a Financiar',
+            input: 'text',
+            showCancelButton: true,
+            inputValidator: (monto) => {
+                let montoFinanciar = parseInt(monto)
+                if(!montoFinanciar || isNaN(montoFinanciar) || montoFinanciar<0){
+                    return 'Debes escribir un número'
+                }
+            }
+                
+        })
+//Solicitamos la cantidad de Cuotas        
+.then((result) => {
+    if(result.isConfirmed){
+        let montoFinanciar = parseInt( result.value );
+        Swal.fire ( {
+            title: 'Ingrese la cantidad de Cuotas',
+            input: 'text',
+            showCancelButton: true,
+            inputValidator: (cuotas) => {
+                let cantCuotas = parseInt(cuotas)
+                if(!cantCuotas || isNaN(cantCuotas) || cantCuotas<0){
+                    return 'Debes escribir un número'
+                }
+            }
+                
+        })
+// Cargamos los Datos del nuevo Boleto        
+.then((result) => {
+    if(result.isConfirmed){
+        let cantCuotas = parseInt( result.value );                                          
+        const objetoUnidad = unidades.find (producto =>{
+            return producto.numeroUnidad === numeroUnidad;
+        })
+        
+        archivoBoletos = JSON.parse(localStorage.getItem('boletos'));
+    
+        let nuevoBoleto = new Boleto (generarBoleto(), objetoUnidad, nombre, montoFinanciar, cantCuotas)
+        archivoBoletos.push(nuevoBoleto); 
+        sincronizarConLocalStorage(archivoBoletos)
+        representaArreglo(archivoBoletos);
+        
+        console.log('Financiación: ')
+        console.log(nuevoBoleto)
+        for( let i=1; i<=cantCuotas; i++){
+            let numeroCuota = i
+            let valorCuota = (montoFinanciar / cantCuotas)* (1+interes) ** (i-1);
+            console.log('Nº Cuota: '+ numeroCuota + ' / Valor de Cuota: ' + valorCuota.toFixed(2))
+        }
+        
+        Swal.fire ( {
+            title: 'Boleto Cargado',
+            icon: 'success',
+            showConfirmButton: false,
+            timer:1500
+        })
+}
+})
+}
+})
+}
+})
+}
+})
+}   
+})
 }
 
 
@@ -148,7 +170,8 @@ const iniciado = () => {
     let storageBoletos = JSON.parse(localStorage.getItem('boletos'));
     storageBoletos == null && sincronizarConLocalStorage (archivoBoletos)
     archivoBoletos = JSON.parse(localStorage.getItem('boletos'));
-    representaArreglo(archivoBoletos);    
+    representaArreglo(archivoBoletos); 
+    representaInmuebles(unidades); 
 };
 
 // Función sincronizar los Boletos
@@ -174,34 +197,68 @@ const limpiarCuerpo = () => {
                 <th>Monto</th>
                 <th>Cuotas</th>
                 <th>Cuota Inicial</th>
-            </tr>
+                <th></th>
             </tr>
         </thead>
-        <tbody id="body">
+        <tbody id="tablaBoletos">
+            <!--Contenido de la tabla-->
+        </tbody>`
+
+        document.getElementById('Tabla2').innerHTML= `
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody id="tablaFinanciación">
             <!--Contenido de la tabla-->
         </tbody>`
 };
 
 
+// Función que muestra los inmuebles en el navBar.
+
+const mostrarInmuebles = (numeroInmueble, datosUnidad) =>{ 
+    document.getElementById('listaUnidades').insertRow(-1).innerHTML = 
+        `<tr>
+            <td>${numeroInmueble}</td>
+            <td>${datosUnidad}</td>
+        </tr>`
+};
+
 // Función que muestra los datos en la tabla.
 
 const mostrarEnTabla = (numeroBoleto, datosUnidad, cliente, montoFinanciar, numeroCuotas) =>{ 
-    document.getElementById('Tabla1').insertRow(-1).innerHTML = 
-        `<tr>
-            <td>${numeroBoleto}</td>
-            <td>${datosUnidad}</td>
-            <td>${cliente}</td>
-            <td>${montoFinanciar.toFixed(2)}</td>
-            <td>${numeroCuotas}</td>
-            <td>${(montoFinanciar / numeroCuotas).toFixed(2)}</td>
-        </tr>`
+    document.getElementById('tablaBoletos').insertRow(-1).innerHTML = 
+        `
+            <tr>
+                <td>${numeroBoleto}</td>
+                <td>${datosUnidad}</td>
+                <td>${cliente}</td>
+                <td>${montoFinanciar.toFixed(2)}</td>
+                <td>${numeroCuotas}</td>
+                <td>${(montoFinanciar / numeroCuotas).toFixed(2)}</td>
+                <td class="text-center delete"><a href="#" id="${numeroBoleto}" class="borrarBoleto" data-id="1">X</a></td>
+                <td class="text-center view"><a href="#" id="${numeroBoleto}" class="verBoleto" data-id="1">?</a></td>
+            </tr>
+        `
 };
+
 
 /*A incorporar mas adelante
 <td class="text-center "><a id="eliminar_${numeroBoleto}"class="borrarBoleto" data-id="1">X</a></td>
 <td class="text-center "><a id="ver_${numeroBoleto}"class="verBoleto" data-id="1">¿?</a></td>
 */
 
+// Función insertar los inmuebles en el NavBar.
+const representaInmuebles = (arreglo) => {
+    arreglo.forEach(objeto => {
+        mostrarInmuebles(objeto.numeroUnidad,`${objeto.torre} ${objeto.piso} ${objeto.unidad}`);
+    });
+    archivoBoletos.splice(0,archivoBoletos.length)
+    
+};
 
 // Función que borra la tabla, luego muestra el arreglo parámetro en ella (indirectamente).
 const representaArreglo = (arreglo) => {
@@ -210,28 +267,72 @@ const representaArreglo = (arreglo) => {
         mostrarEnTabla(objeto.numeroBoleto,[`${objeto.unidad.torre} ${objeto.unidad.piso} ${objeto.unidad.unidad}`],objeto.cliente,objeto.montoFinanciar,objeto.numeroCuotas);
     });
     archivoBoletos.splice(0,archivoBoletos.length)
+
     
-};
+    // Función que elimina un trabajo de impresión de la lista según su ID. Luego actualiza la tabla para mostrarlo (indirectamente).
+
+    let buttonDelete = document.querySelectorAll('#tablaBoletos tr td.delete a');
+    buttonDelete.forEach(element => {
+        element.addEventListener('click',(e)=>{
+            e.preventDefault();
+            let id = e.target.id
+
+            Swal.fire ( {
+                title: '¿Está seguro que quiere eliminarlo??',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: 'Si quiero',
+                cancelButtonText: 'No quiero'
+            }).then((result) => {
+                if(result.isConfirmed){
+                    archivoBoletos = JSON.parse(localStorage.getItem('boletos'));
+                    archivoBoletos.splice(archivoBoletos.indexOf(archivoBoletos.find(elemento => elemento.numeroBoleto == id)),1);
+                    sincronizarConLocalStorage (archivoBoletos)
+                    representaArreglo(archivoBoletos);
+                    
+                    Swal.fire ( {
+                        title: 'Boleto Eliminado',
+                        icon: 'success',
+                        showConfirmButton: false,
+                        timer:1500
+                    })
+                }
+            })
+        })
+    })
 
 
-// Función que elimina un trabajo de impresión de la lista según su ID. Luego actualiza la tabla para mostrarlo (indirectamente).
+    let buttonView = document.querySelectorAll('#tablaBoletos tr td.view a');
+    buttonView.forEach(element => {
+        element.addEventListener('click',(e)=>{
+            e.preventDefault();
+            let id = e.target.id
+            archivoBoletos = JSON.parse(localStorage.getItem('boletos'));
+            let boletoFinanciacion = archivoBoletos.filter(elemento => elemento.numeroBoleto == id );
+            representaArreglo(boletoFinanciacion);
 
-const eliminar = () => {
-    let objetivo = 0;
-    do{
-        objetivo = prompt("Ingrese el índice del registro a eliminar");
-    }while (isNaN(objetivo));
-    archivoBoletos = JSON.parse(localStorage.getItem('boletos'));
-    if(confirm("Está seguro que quiere eliminarlo?")) { //SEGUIR ACA
-        if(!(archivoBoletos.find(elemento => elemento.numeroBoleto == objetivo)===undefined)){
-            archivoBoletos.splice(archivoBoletos.indexOf(archivoBoletos.find(elemento => elemento.numeroBoleto == objetivo)),1);
-            sincronizarConLocalStorage (archivoBoletos)
-            representaArreglo(archivoBoletos);
-            alert("Boleto eliminado");
-        }else{
-            alert("El Boleto no existe");
-        }
-    }
+            document.getElementById('Tabla2').innerHTML= `
+                <thead>
+                    <tr>
+                        <th>Nº Cuota</th>
+                        <th>Valor Cuota</th>
+                    </tr>
+                </thead>
+                <tbody id="tablaFinanciación">
+                    <!--Contenido de la tabla-->
+                </tbody>`
+                for( let i=1; i<=boletoFinanciacion[0].numeroCuotas; i++){
+                    let numeroCuota = i
+                    let valorCuota = (boletoFinanciacion[0].montoFinanciar / boletoFinanciacion[0].numeroCuotas)* (1+interes) ** (i-1);
+                    document.getElementById('tablaFinanciación').insertRow(-1).innerHTML= `
+                        <tr>
+                            <th>${numeroCuota}</th>
+                            <th>${valorCuota.toFixed(2)}</th>
+                        </tr>
+                        `
+                }
+        })
+    }) 
 };
 
 
